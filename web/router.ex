cb33f6ebe314cf0b3173baca37a84b1c7c0caee1
Merrick Kirby <makirby@outlook.com>
2017-07-25 00:31:16 +0100

    Add new resource routes

--- web/router.ex
+++ web/router.ex
@@ -17,8 +17,24 @@ defmodule Ybrikam.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    resources "/users", UserController do
+      resources "/posts", PostController do # only: [:index, :show]
+        resources "/comments", CommentController # except: [:delete]
+      end
+    end
+
+    resources "/reviews", ReviewController
+
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
+  end
+
+  scope "/admin", Ybrikam.Admin, as: :admin do 
+    pipe_through :browser
+
+    resources "/images",  ImageController
+    resources "/reviews", ReviewController
+    resources "/users",   UserController
   end
 
   # Other scopes may use custom stacks.
